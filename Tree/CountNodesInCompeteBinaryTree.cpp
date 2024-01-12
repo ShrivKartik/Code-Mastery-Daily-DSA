dc1e33f78ad5d26e1b4cf1405937da4a7c75c108
ShrivKartik <codewithkartikey2003@gmail.com>
2024-01-13 02:11:45 +0530

    Count Nodes in Complete Binary Tree

--- Tree/CountNodesInCompeteBinaryTree.cpp
+++ Tree/CountNodesInCompeteBinaryTree.cpp
@@ -12,34 +12,32 @@ struct Node
   }
 };
 
-int res=0;
-int burnTime(Node *root, int leaf, int &dist){
-    if(root==NULL)return 0;
-    if(root->key==leaf){dist=0;return 1;}
-    int ldist=-1,rdist=-1;
-    int lh=burnTime(root->left,leaf,ldist);
-    int rh=burnTime(root->right,leaf,rdist);
-    
-    if(ldist!=-1){
-       dist=ldist+1;
-       res=max(res,dist+rh);
+int countNode(Node *root){
+    int lh=0,rh=0;
+    Node *curr=root;
+    while(curr!=NULL){
+        lh++;
+        curr=curr->left;
     }
-    else if(rdist!=-1){
-        dist=rdist+1;
-        res=max(res,dist+lh);
+    curr=root;
+    while(curr!=NULL){
+        rh++;
+        curr=curr->right;
     }
-    return max(lh,rh)+1;
-}
+    if(lh==rh){
+        return pow(2,lh)-1;
+    }else{
+        return 1+countNode(root->left)+countNode(root->right);
+    }
+}  
+
 int main() {
 	
 	Node *root=new Node(10);
 	root->left=new Node(20);
 	root->right=new Node(30);
-	root->left->left=new Node(40);
-	root->left->right=new Node(50);
-	root->left->left->left=new Node(60);
-	root->left->left->left->left=new Node(70);
-	int dist=-1;int leaf=50;
-	burnTime(root,leaf,dist);
-	cout<<res;
+	root->right->left=new Node(40);
+	root->right->right=new Node(50);
+	
+	cout<<countNode(root);
 }
